0fba917f185cd4040d4a7e6590d5c973923543dd
Halima Gueye <halima.gueye@gmail.com>
2022-08-31 13:46:03 +0000

    firebase credentials updated

--- App.js
+++ App.js
@@ -1,17 +1,2 @@
-import React from 'react';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-
-import { RootNavigator } from './navigation/RootNavigator';
-import { AuthenticatedUserProvider } from './providers';
-
-const App = () => {
-  return (
-    <AuthenticatedUserProvider>
-      <SafeAreaProvider>
-        <RootNavigator />
-      </SafeAreaProvider>
-    </AuthenticatedUserProvider>
-  );
-};
-
-export default App;
+import App from './src/index'
+export default App
---
+++ src/config.js
@@ -0,0 +1,13 @@
+const defaultAvatar = 'https://firebasestorage.googleapis.com/v0/b/objectives-90f9c.appspot.com/o/icon.png?alt=media&token=3a49a8db-56ab-49ba-9c5d-e404f2261ae5'
+const firebaseKey = {
+  apiKey: 'AIzaSyBa_cQyraoFKLEED6BGpWa2OsDKFCx8As8',
+  authDomain: 'objectives-90f9c.firebaseapp.com',
+  projectId: 'objectives-90f9c',
+  storageBucket: 'objectives-90f9c.appspot.com',
+  messagingSenderId: '976139332875',
+  appId: '1:976139332875:web:b0a92083a52eee3dce460e',
+}
+
+const eulaLink = 'https://github.com/kiyohken2000/ReactNative-Expo-Firebase-Boilerplate-v2'
+
+export { defaultAvatar, firebaseKey, eulaLink }
